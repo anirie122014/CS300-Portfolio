16232efaf64c1435fe0c258e142d7961dd4c7c43
anirie122014 <aniri.estrella@snhu.edu>
2026-04-23 11:09:17 -0400

    Add files via upload

---
+++ ProjectTwo.cpp
@@ -0,0 +1,171 @@
+// Name:  Project ABCU Adivising Program
+// Arthur: Aniri Estrella
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// store information
+
+struct Course {
+    string courseNumber;
+    string courseTitle;
+    vector<string> prerequisites;
+};
+// each node holds a course and pointers to left and right children
+struct Node {
+    Course course;
+    Node* left;
+    Node* right;
+
+    Node(Course c) {
+        course = c;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+// organzie and store courses
+// allows insertion, search, and printing in sorted order
+class BinarySearchTree {
+private:
+    Node* root;
+
+    void addNode(Node* node, Course course);
+    void inOrder(Node* node);
+    Course search(Node* node, string courseNumber);
+
+public:
+    BinarySearchTree();
+    void Insert(Course course);
+    void PrintAllCourses();
+    Course Search(string courseNumber);
+};
+
+BinarySearchTree::BinarySearchTree() {
+    root = nullptr;
+}
+// insert a course into the Binary Search Tree
+// Smallest course number to the left, largest to the right
+void BinarySearchTree::Insert(Course course) {
+    if (root == nullptr) {
+        root = new Node(course);
+    }
+    else {
+        addNode(root, course);
+    }
+}
+// Finds correct position in the tree
+void BinarySearchTree::addNode(Node* node, Course course) {
+    if (course.courseNumber < node->course.courseNumber) {
+        if (node->left == nullptr) {
+            node->left = new Node(course);
+        }
+        else {
+            addNode(node->left, course);
+        }
+    }
+    else {
+        if (node->right == nullptr) {
+            node->right = new Node(course);
+        }
+        else {
+            addNode(node->right, course);
+        }
+    }
+}
+// Prints all courses in sorted order, uses in order traversal
+void BinarySearchTree::PrintAllCourses() {
+    inOrder(root);
+}
+// this prints sorted data
+void BinarySearchTree::inOrder(Node* node) {
+    if (node == nullptr) {
+        return;
+    }
+
+    inOrder(node->left);
+    cout << node->course.courseNumber << ", " << node->course.courseTitle << endl;
+    inOrder(node->right);
+}
+// searches for a course by course number 
+Course BinarySearchTree::Search(string courseNumber) {
+    return search(root, courseNumber);
+}
+
+Course BinarySearchTree::search(Node* node, string courseNumber) {
+    if (node == nullptr) {
+        return Course();
+    }
+
+    if (node->course.courseNumber == courseNumber) {
+        return node->course;
+    }
+
+    if (courseNumber < node->course.courseNumber) {
+        return search(node->left, courseNumber);
+    }
+    else {
+        return search(node->right, courseNumber);
+    }
+}
+
+void loadCourses(string fileName, BinarySearchTree& bst) {
+
+    ifstream file(fileName);
+
+    if (!file.is_open()) {
+        cout << "Error: File could not be opened." << endl;
+        return;
+    }
+
+    string line;
+
+    while (getline(file, line)) {
+
+        stringstream ss(line);
+        string token;
+
+        Course course;
+
+        // Course Number
+        getline(ss, token, ',');
+        course.courseNumber = token;
+
+        // Course Title
+        getline(ss, token, ',');
+        course.courseTitle = token;
+
+        // Prerequisites
+        while (getline(ss, token, ',')) {
+            course.prerequisites.push_back(token);
+        }
+
+        bst.Insert(course);
+    }
+
+    file.close();
+
+    cout << "Courses loaded successfully." << endl;
+}
+
+int main() {  //prompt user for file name, loads course data and prints all
+
+    BinarySearchTree bst;
+    string fileName;
+
+    cout << "Enter file name (include .csv): ";
+    getline(cin >> ws, fileName);
+
+    loadCourses(fileName, bst);
+
+    bst.PrintAllCourses();
+
+    return 0;
+}
